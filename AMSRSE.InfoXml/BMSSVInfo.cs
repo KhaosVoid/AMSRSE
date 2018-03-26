75eb91b107f7c01eb7f3f38f49be9fbe7f0c92ec
KhaosVoid <ztommey@gmail.com>
2018-03-26 02:21:25 -0500

    Beginning progress of creating a data viewer for the save format. This will assist in discovering what other chunks do in the save files.

---
+++ AMSRSE.InfoXml/BMSSVInfo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AMSRSE.InfoXml
+{
+    public class BMSSVInfo
+    {
+        #region Dependency Properties
+
+        public static readonly DependencyProperty BlockNamesProperty =
+            DependencyProperty.Register("BlockNames", typeof(IList<Name>), typeof(BMSSVInfo));
+
+        #endregion Dependency Properties
+
+        #region Ctor
+
+        public BMSSVInfo()
+        {
+
+        }
+
+        #endregion Ctor
+
+        #region Methods
+
+        public static BMSSVInfo Load(string path)
+        {
+            return null;
+        }
+
+        #endregion Methods
+    }
+}
---
+++ AMSRSE.InfoXml/Name.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AMSRSE.InfoXml
+{
+    public class Name : DependencyObject
+    {
+        #region Dependency Properties
+
+        public static readonly DependencyProperty IdProperty =
+            DependencyProperty.Register("Id", typeof(string), typeof(Name));
+
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.Register("Value", typeof(string), typeof(Name));
+
+        #endregion Dependency Properties
+
+        #region Properties
+
+        public string Id
+        {
+            get { return (string)GetValue(IdProperty); }
+            set { SetValue(IdProperty, value); }
+        }
+
+        public string Value
+        {
+            get { return (string)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+
+        #endregion Properties
+
+        #region Ctor
+
+        public Name()
+        {
+
+        }
+
+        #endregion Ctor
+    }
+}
